122703cc8897943d944eae84f2c1b60d58d52504
yahya <yahya@192.168.1.18>
2020-06-02 23:55:50 +0300

    ch6_7

---
+++ src/main/java/com/yahya/ch4HW/Ch4HwApplication.java
@@ -0,0 +1,13 @@
+package com.yahya.ch4HW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch4HwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yahya/ch4HW/Ch4HwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahya.ch4HW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch4HwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
